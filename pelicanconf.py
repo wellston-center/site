16923b977dd6e81ef816d777a036ba9355f969e8
Thomas Kent <tom@teeks99.com>
2019-04-11 20:58:52 -0500

    Stole config from momoney and customized

---
+++ pelicanconf.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Wellston Center'
+SITENAME = 'St. Augustine Wellston Center'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+STATIC_PATHS = ['img']
+
+FAVICON = 'img/favicon.ico'
+MENUITEMS = [
+    ("Home", "index.html"),
+    ("About", "about.html"),
+    ("Contact", "contact.html")
+]
+DISPLAY_PAGES_ON_MENU = False
+
+#THEME = 'theme/notmyidea'
+#THEME = 'theme/pelican-bootstrap3'
+#PLUGIN_PATHS = ['plugins', ]
+#PLUGINS = ['i18n_subsites', ]
+#JINJA_ENVIRONMENT = {
+#    'extensions': ['jinja2.ext.i18n'],
+#}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
